660dd36d540b5f72055276e551cb13673aa1aae7
Alex Walburg <ajwalburg@gmail.com>
2022-03-27 01:01:34 -0700

    added in some defaults like doubles
    My girlfriend plays doubles and she asked if I could change the
    defaults to be more vgc like. Since I'm basically making this for her
    I figured I could oblige her on the publishing branch.

--- docs/js/shared_controls.js
+++ docs/js/shared_controls.js
@@ -1006,7 +1006,7 @@ $(".notation").change(function () {
 });
 
 function clearField() {
-	$("#singles-format").prop("checked", true);
+	$("#doubles-format").prop("checked", true);
 	$("#clear").prop("checked", true);
 	$("#gscClear").prop("checked", true);
 	$("#gravity").prop("checked", false);
